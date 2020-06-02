7a2135fca0d77f9a194227d5412731803e351a09
nurulid <nurulinsanidewi@ymail.com>
2020-06-02 23:56:42 +0700

    Footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,19 +1,41 @@
 import React from "react";
 import styled from "styled-components";
-import Logo from "./small-components/Logo"
+import Logo from "./small-components/Logo";
 
 const FooterWrapper = styled.footer`
-  height: 200px;
+  min-height: 200px;
   background-color: hsl(216, 53%, 9%);
   padding: 60px 0;
-  .footer-wrapper{
+  font-size: 16px;
+  Logo {
+    margin-bottom: 30px;
+  }
+  .footer-wrapper {
     width: 80%;
     margin: 0 auto;
     height: 100%;
   }
   .footer-details {
-    display: flex;
-    justify-content: space-between;
+    display: grid;
+    grid-template-columns: 4fr 2fr 1fr 1fr 1fr;
+    grid-gap: 50px;
+    margin-top: 15px;
+    ul {
+      padding: 0;
+      list-style: none;
+      li {
+        margin-bottom: 15px;
+      }
+    }
+  }
+
+  @media screen and (max-width: 768px) {
+    .footer-details {
+      display: block;
+      & > div {
+        margin-bottom: 30px;
+      }
+    }
   }
 `;
 
@@ -21,13 +43,52 @@ const Footer = () => {
   return (
     <FooterWrapper>
       <div className="footer-wrapper">
-        <Logo/>
+        <Logo />
         <div className="footer-details">
-          <div>1</div>
-          <div>1</div>
-          <div>1</div>
-          <div>1</div>
-          <div>1</div>
+          <div>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Fugit odio consequuntur
+            deserunt accusamus numquam ullam dolore cumque, adipisci.
+          </div>
+          <div>
+            <ul>
+              <li>+1-456-2423-342</li>
+              <li>example@fylo.com</li>
+            </ul>
+          </div>
+          <div>
+            <ul>
+              <li>
+                <a href="">About Us</a>
+              </li>
+              <li>
+                <a href="">Jobs</a>
+              </li>
+              <li>
+                <a href="">Press</a>
+              </li>
+              <li>
+                <a href="">Blog</a>
+              </li>
+            </ul>
+          </div>
+          <div>
+            <ul>
+              <li>
+                <a href="">Contact Us</a>
+              </li>
+              <li>
+                <a href="">Terms</a>
+              </li>
+              <li>
+                <a href="">Privacy</a>
+              </li>
+            </ul>
+          </div>
+          <div>
+            <a href="">fb</a>
+            <a href="">twitter</a>
+            <a href="">ig</a>
+          </div>
         </div>
       </div>
     </FooterWrapper>
--- src/components/SectionAccess.js
+++ src/components/SectionAccess.js
@@ -12,7 +12,7 @@ const SectionAccessWrapper = styled.section`
     text-align: center;
     padding: 30px;
     border-radius: 4px;
-    box-shadow: 0 .5rem 1rem rgba(0,0,0,.15);
+    box-shadow: 0 0.5rem 1rem rgba(0, 0, 0, 0.15);
     h2 {
       margin-top: 0;
       font-size: 35px;
@@ -25,9 +25,9 @@ const SectionAccessWrapper = styled.section`
       padding: 12px 20px;
       border-radius: 25px;
       border: unset;
-      margin-right: 20px;
+      /* margin-right: 20px; */
       width: 70%;
-      :active{
+      :active {
         outline: none;
       }
     }
@@ -35,9 +35,26 @@ const SectionAccessWrapper = styled.section`
   .sign-up-wrapper {
     display: flex;
     box-sizing: border-box;
-    justify-content: center;
+    justify-content: space-between;
     align-items: center;
     padding: 0 50px;
+  }
+
+  @media screen and (max-width: 768px) {
+    .access-wrapper {
+      width: 90%;
+      p {
+        padding: 0;
+      }
+      input {
+        width: 100%!important;
+        margin-bottom: 15px;
+      }
+    }
+    .sign-up-wrapper {
+      display: block;
+      padding: 0;
+    }
   }
 `;
 
@@ -47,10 +64,12 @@ const SectionAccess = () => {
       <div className="access-wrapper">
         <h2>Get early access today</h2>
         <p>
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Rem dolorum nobis ut incidunt tempore et, commodi illum. Enim id iusto ab nihil facere incidunt quibusdam, eaque laboriosam.
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Rem dolorum nobis ut incidunt
+          tempore et, commodi illum. Enim id iusto ab nihil facere incidunt quibusdam, eaque
+          laboriosam.
         </p>
         <div className="sign-up-wrapper">
-          <input type="text" className="input"/>
+          <input type="text" className="input" />
           <Button>Get Started For Free</Button>
         </div>
       </div>
